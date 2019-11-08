c04e3d906100239b015f8ee92283b536e0b78c03
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-11-08 18:19:06 +0700

    Task1
    This reverts commit d082e9d8

--- src/ru/progwards/alex/WrongClass.java
+++ src/ru/progwards/alex/WrongClass.java
@@ -1,5 +1,19 @@
 package ru.progwards.alex;
 
 public class WrongClass {
+//    String str1="Хорошо идут дела";
+//    String str2 = "Изучаю Java я!";
+//    String str3 = " ";
+//
+//        System.out.println(str1);
+//        System.out.println(str2);
+//        System.out.print(str1);
+//        System.out.print(str3);
+//        System.out.println(str2);
+//
+//
+//        System.out.print(str2);
+//        System.out.print(str3);
+//        System.out.print(str1);
 
 }
