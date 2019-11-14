e259b192f2bbdce6fc5cd5575d53c99237f94fec
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-11-14 20:07:41 +0700

    H2 2

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -4,7 +4,7 @@ public class AccuracyDoubleFloat {
 
     public static double volumeBallDouble(double radius){
         Double pi=3.14;
-        Double V = 4/(3*pi*radius*radius*radius);
+        Double V = 4/3*pi*(radius*radius*radius);
         return V;
     }
 
