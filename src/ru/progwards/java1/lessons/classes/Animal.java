58e8cb3d3da511e314a61b0432812a1f85029b49
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-11-18 00:27:13 +0700

    masaj22bot

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -1,21 +1,38 @@
-package ru.progwards.java1.lessons.classes;
-enum AnimalKind {
-    ANIMAL,
-    COW,
-    HAMSTER,
-    DUCK
-}
-class Animal {
-
-    public void getKind() {
-        AnimalKind a = AnimalKind.ANIMAL;
-        System.out.println(a);
-    }
-    public static void main(String[] args) {
-        Animal a=new Animal();
-        a.getKind();
-
-    }
-
-
-}
+//package ru.progwards.java1.lessons.classes;
+//enum AnimalKind {
+//    ANIMAL,
+//    COW,
+//    HAMSTER,
+//    DUCK
+//}
+//enum FoodKind {
+//    UNKNOWN,
+//    HAY,
+//    CORN
+//}
+//
+//class Animal {
+//
+//    public void getKind() {
+//        AnimalKind a = AnimalKind.ANIMAL;
+//        System.out.println(a);
+//    }
+//
+//class FoodK{
+//
+//      public void getFoodKind() {
+//            FoodKind f = FoodKind.UNKNOWN;
+//            System.out.println(f);
+//        }
+//
+//public static void main(String[] args) {
+//      Animal a = new Animal();
+//      a.getKind();
+//
+//      FoodK f1 = new FoodK();
+//      f1.getFoodKind();
+//
+//    }
+//}
+//
+//}
