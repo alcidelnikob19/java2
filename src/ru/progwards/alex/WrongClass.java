3ba0b86daea14829d76775d4e3634d381080af2e
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-11-20 19:12:08 +0700

    H4

--- src/ru/progwards/alex/WrongClass.java
+++ src/ru/progwards/alex/WrongClass.java
@@ -1,5 +1,8 @@
 package ru.progwards.alex;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import ru.progwards.java1.telegrambot.ProgwardsTelegramBot;
+
 public class WrongClass {
 //    String str1="Хорошо идут дела";
 //    String str2 = "Изучаю Java я!";
@@ -17,3 +20,39 @@ public class WrongClass {
 //        System.out.print(str1);
 
 }
+
+
+
+
+
+
+
+
+
+
+//package ru.progwards.java1.testlesson;
+//        import org.telegram.telegrambots.ApiContextInitializer;
+//        import ru.progwards.java1.telegrambot.ProgwardsTelegramBot;
+//        import java.util.Scanner;
+//
+//public class PizzaBot extends ProgwardsTelegramBot {
+//    public String processMessage(String text){
+//        checkTags(text);
+//        return "Под твой запрос подходит: \n" ;
+//    }
+//
+//
+//
+//    public static void main(String[] args){
+//        System.out.println("Hello bot!");
+//        ApiContextInitializer.init();
+//
+//        ru.progwards.java1.testlesson.PizzaBot bot = new ru.progwards.java1.testlesson.PizzaBot();
+//        bot.username = "masaj22bot";
+//        bot.token = "922149229:AAFP8Fhmjii6fNOqCVuNdXA5ny6urOPSCFk";
+//        bot.addTags("Массаж Тайский", "Релакс, Разминание");
+//        bot.addTags("Массаж Казачий", "Болевое воздействие , Растягивание");
+//        bot.addTags("Массаж Японский", "Шиацу, скручивание, вытягиваине");
+//        bot.start();
+//    }
+//}
