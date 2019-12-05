aef7ab0b40e1ffa6527bb6d93bdb892ea2799b4d
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-12-05 22:11:49 +0700

    H5

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -12,7 +12,7 @@ public class Binary {
     public String toString() {
         int n = num & 0x7F_FF_FF_FF;
         String r = num < 0 ? "1" : "0";
-        for(int i = 1; i < 32; i++) {
+        for(int i = 1; i < 8; i++) {
             r += (n & 0x40_00_00_00) == 0x40_00_00_00 ? "1" : "0";
             //if(i % 4 == 3) r += " ";
             n <<= 1;
