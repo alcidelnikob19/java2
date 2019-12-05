9ad86e0ae3e6c12a2c745e27ef933d5bf4fdc8a8
alcidelnikob19 <54812103+alcidelnikob19@users.noreply.github.com>
2019-12-05 07:58:47 +0700

    H5

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -4,16 +4,16 @@ public class CheckBit {
 
     // возвращает значение бита с порядковым номером bitNumber от параметра value
     public static int checkBit(byte value, int bitNumber) {
-        return value >>> bitNumber & 1;
+        return value >> bitNumber & 1;
     }
 
     public static void main(String[] args) {
-        System.out.println("0=" + checkBit((byte)0b0000_0000, 7));
-        System.out.println("1=" + checkBit((byte)0b1000_0000, 7));
-        System.out.println("0=" + checkBit((byte)0b0000_0000, 4));
-        System.out.println("1=" + checkBit((byte)0b0001_0000, 4));
-        System.out.println("0=" + checkBit((byte)0b1000_0000, 4));
-        System.out.println("1=" + checkBit((byte)0b1001_0000, 4));
+        System.out.println(checkBit((byte)0b0000_0000, 7));
+        System.out.println(checkBit((byte)0b1000_0000, 0));
+        System.out.println(checkBit((byte)0b0000_0000, 4));
+        System.out.println(checkBit((byte)0b0001_0000, 4));
+        System.out.println(checkBit((byte)0b1000_0000, 4));
+        System.out.println(checkBit((byte)0b1001_0000, 4));
     }
 
 }
